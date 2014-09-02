2569b769028557ff85829c9286f2d6382fb0cfed
kieranpblack <kieranblack@wsu-30.local>
2014-09-02 16:13:08 +1000

    Initial

---
+++ deleteLayerByID.jsx
@@ -0,0 +1,7 @@
+function deleteLayerByID(id) {
+  var desc = new ActionDescriptor();
+  var ref = new ActionReference();
+  ref.putIdentifier(charIDToTypeID('Lyr '), id);
+  desc.putReference( charIDToTypeID('null'), ref );
+  executeAction( charIDToTypeID('Dlt '), desc, DialogModes.NO );
+};
