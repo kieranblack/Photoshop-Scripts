5857b2fc7a68c66f7b79970a6a5fd8af9cd462da
kieranpblack <kieranpblack@gmail.com>
2014-09-02 16:31:47 +1000

    Update getActiveLayerID.jsx

--- getActiveLayerID.jsx
+++ getActiveLayerID.jsx
@@ -1,6 +1,6 @@
-function getActiveLayerID() {   
-    var ref = new ActionReference();   
-    ref.putProperty( charIDToTypeID("Prpr") , charIDToTypeID( "LyrI" ));   
-    ref.putEnumerated( charIDToTypeID("Lyr "), charIDToTypeID("Ordn"), charIDToTypeID("Trgt") );  
-	return executeActionGet(ref).getInteger( stringIDToTypeID( "layerID" ) );  
+function getActiveLayerID(){
+	var ref = new ActionReference();
+	ref.putProperty( charIDToTypeID("Prpr") , charIDToTypeID( "LyrI" ));
+	ref.putEnumerated( charIDToTypeID("Lyr "), charIDToTypeID("Ordn"), charIDToTypeID("Trgt") );
+	return executeActionGet(ref).getInteger( stringIDToTypeID( "layerID" ) );
 }
