1b29ffd2195150d95db5b96f1efd66e74af1884b
kieranpblack <kieranpblack@gmail.com>
2014-09-02 16:47:31 +1000

    Update getLayerVisibilityByID.jsx

--- getLayerVisibilityByID.jsx
+++ getLayerVisibilityByID.jsx
@@ -1,6 +1,6 @@
-function getLayerVisibilityByID(id) { 
-    var ref = new ActionReference(); 
-    ref.putProperty( charIDToTypeID("Prpr") , charIDToTypeID( "Vsbl" )); 
-    ref.putIdentifier( charIDToTypeID( "Lyr " ), id);
-    return executeActionGet(ref).getBoolean(charIDToTypeID( "Vsbl" ));; 
+function getLayerVisibilityByID(id){ 
+  var ref = new ActionReference(); 
+  ref.putProperty( charIDToTypeID("Prpr") , charIDToTypeID( "Vsbl" )); 
+  ref.putIdentifier( charIDToTypeID( "Lyr " ), id);
+  return executeActionGet(ref).getBoolean(charIDToTypeID( "Vsbl" ));; 
 };
